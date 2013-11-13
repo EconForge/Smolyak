6552b54da4b52777db42ffdfa2a4c57cbb91da16
chase <cc7768@gmail.com>
2013-11-13 12:58:26 -0500

    PY: Cleaned up the python files.  Moved all functions in class to top level functions.  Deleted old attempts at optimizing it.

--- Python/smolyak_utils.py
+++ Python/smolyak_utils.py
@@ -1,35 +1,6 @@
 import numpy as np
 
 
-def cheby_eval(x, n):
-    past_val = np.ones_like(x)
-    curr_val = x
-
-    if n == 1:
-        return past_val
-
-    if n == 2:
-        return curr_val
-
-    for i in xrange(3, n + 1):
-        temp = 2*x*curr_val - past_val
-        past_val = curr_val
-        curr_val = temp
-
-    return curr_val
-
-
-def cheby2n(x, n):
-    # computes the chebychev polynomials of the first kind
-    dim = x.shape
-    results = np.zeros((n+1, ) + dim)
-    results[0, ...] = np.ones(dim)
-    results[1, ...] = x
-    for i in range(2,n+1):
-        results[i,...] = 2 * x * results[i-1,...] - results[i-2,...]
-    return results
-
-
 def permute(a):
     """
     Creates all unique combinations of a list a that is passed in.
@@ -74,25 +45,3 @@ def permute(a):
 
                 # yield list(a)
                 return
-
-
-def num_grid_points(d, mu):
-    if mu == 1:
-        return 2*d + 1
-
-    if mu == 2:
-        return 1 + 4*d + 4*d*(d-1)/2.
-
-    if mu == 3:
-        return 1 + 8*d + 12*d*(d-1)/2. + 8*d*(d-1)*(d-2)/6.
-
-
-def m_i(i):
-    if i < 0:
-        raise ValueError('i must be positive')
-    elif i == 0:
-        return 0
-    elif i == 1:
-        return 1
-    else:
-        return 2**(i - 1) + 1
