b419e0da2bff5f728ef381352b6d1321d978a54e
chase <cc7768@gmail.com>
2013-12-02 23:21:44 -0500

    INTERP: Things are broken for more general domains.  Figure out how to approach.

--- smolyak/interp.py
+++ smolyak/interp.py
@@ -34,8 +34,32 @@ class SmolyakInterp(object):
     """
     def __init__(self, sg, f_on_grid):
         self.sg = sg
-        self.f_on_grid = f_on_grid
+        self.f_on_grid = self.trans_vals(f_on_grid)
         self.theta = find_theta(sg, f_on_grid)
+
+    def trans_vals(self, pts):
+        """
+        Takes the y-values that are being passed in and moves them into
+        the correct domain of [-1, 1].  Note that pts should be 1 dim
+        """
+        low_bounds = np.min(pts)
+        up_bounds = np.max(pts)
+
+        self.centers = low_bounds + (up_bounds - low_bounds)/2
+        self.radii = (up_bounds - low_bounds)/2
+
+        trans_pts = (pts-self.centers)/self.radii
+
+        return trans_pts
+
+    def inv_trans_vals(self, pts):
+        """
+        Takes transformed y-values and moves them back into the original
+        domain for the interpolation values.
+        """
+        inv_trans_vals = pts*self.radii + self.centers
+
+        return inv_trans_vals
 
     def interpolate(self, pts, with_deriv=False):
         """
@@ -70,9 +94,10 @@ class SmolyakInterp(object):
         # Give ourselves the value of theta
         theta = self.theta
         # Move points to correct domain
-        trans_points = sg.trans_points(pts)
+        trans_points = self.sg.trans_points(pts)
 
         new_B = build_B(d, sg.mu, trans_points, sg.pinds)
         vals = new_B.dot(theta)
+        vals = self.inv_trans_vals(vals)
         return vals
 
