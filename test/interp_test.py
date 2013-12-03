7882e2277f267114fd4acab851bf42136f31cdd8
Spencer Lyon <spencerlyon2@gmail.com>
2013-12-02 20:06:53 -0500

    TEST: interp_test.py imports correct thigns

--- test/interp_test.py
+++ test/interp_test.py
@@ -1,29 +1,34 @@
 # TESTS: Leaving these here for now so that I don't have to do anything
 # fancy
+from smolyak.grid import SmolyakGrid
+from smolyak.interp import SmolyakInterp
+import numpy as np
 
+func = lambda x, y: np.exp(x**2 - y**2)
 
-def func(x, y):
-    return np.exp(x**2 - y**2)
+func1 = lambda points: func(points[:, 0], points[:, 1])
 
-def func1(points):
-    return func(points[:, 0], points[:, 1])
+def test_interp_2d(d, mu, f):
+    sg = SmolyakGrid(d, mu, np.array([-1, -1]), np.array([1, 1]))
 
-sg = SmolyakGrid(2, 3, np.array([-1, -1]), np.array([1, 1]))
+    f_on_grid = f(sg.grid)
 
-f_on_grid = func1(sg.grid)
+    si = SmolyakInterp(sg, f_on_grid)
 
-si = SmolyakInterp(sg, f_on_grid)
+    np.random.seed(42)
+    test_points = np.random.randn(100, 2)
+    # Make sure it is bounded by -1, 1
+    test_points = test_points/np.max(np.abs(test_points))
 
-test_points = np.random.randn(100, 2)
-# Make sure it is bounded by -1, 1
-test_points = test_points/np.max(np.abs(test_points))
+    true_vals = f(test_points)
+    interp_vals = si.interpolate(test_points)
 
-true_vals = func1(test_points)
-interp_vals = si.interpolate(test_points)
+    mean_ad = np.mean(np.abs(interp_vals - true_vals))
+    max_ad = np.max(np.abs(interp_vals - true_vals))
+    min_ad = np.min(np.abs(interp_vals - true_vals))
 
-mean_ad = np.mean(np.abs(interp_vals - true_vals))
-max_ad = np.max(np.abs(interp_vals - true_vals))
-min_ad = np.min(np.abs(interp_vals - true_vals))
+    print("The mean abs diff is {}\nThe max abs diff is {}\nThe min abs diff is {}"
+          .format(mean_ad, max_ad, min_ad))
+    return
 
-print("The mean abs diff is {} \nThe max abs diff is {}\n The min abs diff is{}"\
-      .format(mean_ad, max_ad, min_ad))
+test_interp_2d(2, 3, func1)
